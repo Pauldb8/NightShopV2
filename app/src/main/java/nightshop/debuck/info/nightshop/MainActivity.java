fd42c3ac37a41da1847c5362c650019a2be76cf8
Steeves Obiang Ndam <steevesobiang@gmail.com>
2017-11-16 23:58:39 +0100

    getLocalisation

--- app/src/main/java/nightshop/debuck/info/nightshop/MainActivity.java
+++ app/src/main/java/nightshop/debuck/info/nightshop/MainActivity.java
@@ -1,30 +1,55 @@
 package nightshop.debuck.info.nightshop;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import nightshop.debuck.info.nightshop.AppClass.Building;
 import nightshop.debuck.info.nightshop.Tools.BuildingAdapter;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
 
     private List<Building> buildingList = new ArrayList<>();
     private RecyclerView recyclerView;
     private BuildingAdapter mAdapter;
+    private LocationManager locationManager;
+    private double lat;
+    private double lng;
+    private String city;
+    private Location location;
+
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+    private boolean canGetLocation;
 
 
     @Override
@@ -34,14 +59,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
@@ -58,11 +76,24 @@ public class MainActivity extends AppCompatActivity {
         PrepareBuildingData();
 
 
+       // getLocalisation();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "We check your localisation", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                getLocalisation();
+            }
+        });
+
+
     }
 
 
     private void PrepareBuildingData() {
-        for(int i = 0; i < 20; i++) {
+        for (int i = 0; i < 20; i++) {
             Building building = new Building(1, "Shell", "La hulpe qqpart", "Description de l'endroit en question",
                     50.7327087, 4.4787191, 0, 9, 0, 23, 30);
             buildingList.add(building);
@@ -90,5 +121,110 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * ini var Lat, Lng and city
+     */
+
+    public void getLocalisation() {
+
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+        // getting GPS status
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        // getting network status
+        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!isGPSEnabled && !isNetworkEnabled) {
+            // no GPS Provider and no network provider is enabled
+            Toast.makeText(this,"Please enable your GPS or data Network",Toast.LENGTH_LONG).show();
+        } else {   // Either GPS provider or network provider is enabled
+
+            // First get location from Network Provider
+            if (isNetworkEnabled) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES,
+                  //      MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                if (locationManager != null)
+                {
+                   Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if (location != null)
+                    {
+                        lat = location.getLatitude();
+                        lng = location.getLongitude();
+                        this.canGetLocation = true;
+                    }
+                }
+            }// End of IF network enabled
+
+            // if GPS Enabled get lat/long using GPS Services
+            if (isGPSEnabled)
+            {
+                locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                if (locationManager != null)
+                {
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null)
+                    {
+                        lat = location.getLatitude();
+                        lng = location.getLongitude();
+                        this.canGetLocation = true;
+                    }
+                }
+                Log.i("Location Changed", lat + " and " + lng);
+
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> addresses = null;
+                try {
+                    addresses = geocoder.getFromLocation(lat, lng, 100);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String cityName = addresses.get(0).getAddressLine(0);
+                String stateName = addresses.get(0).getAddressLine(1);
+                String countryName = addresses.get(0).getAddressLine(2);
+
+                Log.i("GEOCODER", cityName + " and " + stateName+" and "+countryName);
+
+            }// End of if GPS Enabled
+        }// End of Either GPS provider or network provider is enabled
+
+
+
+    }
+
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            Log.i("Location onChanged", location.getLatitude() + " and " + location.getLongitude());
+        }else{
+            Log.i("Location KO","Location KO");
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
