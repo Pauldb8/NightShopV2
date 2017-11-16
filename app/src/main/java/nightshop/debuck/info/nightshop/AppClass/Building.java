b96e30de94c9d7d4d476e4df150a1f29087607ef
Steeves Obiang Ndam <steevesobiang@gmail.com>
2017-11-16 19:50:43 +0100

    add hours

--- app/src/main/java/nightshop/debuck/info/nightshop/AppClass/Building.java
+++ app/src/main/java/nightshop/debuck/info/nightshop/AppClass/Building.java
@@ -19,11 +19,16 @@ public class Building implements Serializable {
     private double lat;
     private double lng;
     private double distance;
+    private int hour_open;
+    private  int minute_open;
+    private int hour_end;
+    private int minute_end;
 
 
     public Building(){}
 
-    public Building(int id, String name, String address, String description, double lat, double lon, double distance) {
+    public Building(int id, String name, String address, String description, double lat, double lon,
+                    double distance, int hour_open,int minute_open,int hour_end, int minute_end) {
         this.id = id;
         this.name = name;
         this.address=address;
@@ -31,6 +36,10 @@ public class Building implements Serializable {
         this.lng =lon;
         this.description=description;
         this.distance=distance;
+        this.hour_open=hour_open;
+        this.minute_open=minute_open;
+        this.hour_end=hour_end;
+        this.minute_end=minute_end;
     }
 
     public int getId() {
@@ -71,6 +80,22 @@ public class Building implements Serializable {
     public  void setDistance(double distance){ this.distance = distance;}
 
 
+    public int getHour_open() {return hour_open;}
+
+    public void setHour_open(int hour_open) {this.hour_open = hour_open;}
+
+    public int getMinute_open() {return minute_open;}
+
+    public void setMinute_open(int minute_open) {this.minute_open = minute_open;}
+
+    public int getHour_end() {return hour_end;}
+
+    public void setHour_end(int hour_end) {this.hour_end = hour_end;}
+
+    public int getMinute_end() {return minute_end;}
+
+    public void setMinute_end(int minute_end) {this.minute_end = minute_end;}
+
     public void populateObject(Cursor c){
         try{
             this.setId(c.getInt(
@@ -93,7 +118,13 @@ public class Building implements Serializable {
 
     public String toString(){
         return ("[Building: id=" + getId() + ", name=" + getName() +", address=" +
-                getAddress() + ", lat=" + getLat()+ ", lng=" + getLng()+ ", description=" + getDescription() + ", distance=" + getDistance()+"]");
+                getAddress() + ", lat=" + getLat()+ ", lng=" + getLng()+ ", description=" +
+                getDescription() + ", distance=" + getDistance()
+                + ", hour_open=" + getHour_open()
+                + ", minute_open=" + getMinute_open()
+                + ", hour_end=" + getHour_end()
+                + ", minute_end=" + getMinute_end()
+                +"]");
     }
 
 }
